3e36a2997d68549d78f46aaea571614ebb6aff15
buonviyeuanh456@gmail.com <buonviyeuanh456@gmail.com>
2021-06-26 13:18:45 +0700

    Upload Project first time

---
+++ Startup_ShopOnline/Startup_ShopOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Startup_ShopOnline.Startup))]
+namespace Startup_ShopOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
