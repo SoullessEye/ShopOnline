bafc6413a3e1a124896d492bb935550d2a6469c2
buonviyeuanh456@gmail.com <buonviyeuanh456@gmail.com>
2021-06-28 21:48:28 +0700

    Edit and Delete

--- Startup_ShopOnline/Startup_ShopOnline/Areas/Admin/Controllers/NhaSanXuatAdminController.cs
+++ Startup_ShopOnline/Startup_ShopOnline/Areas/Admin/Controllers/NhaSanXuatAdminController.cs
@@ -13,7 +13,7 @@ namespace Startup_ShopOnline.Areas.Admin.Controllers
         // GET: Admin/NhaSanXuatAdmin
         public ActionResult Index()
         {
-            var ds = NhaSanXuatBUS.DanhSach();
+            var ds = NhaSanXuatBUS.DanhSachAdmin();
             return View(ds);
         }
 
@@ -46,19 +46,19 @@ namespace Startup_ShopOnline.Areas.Admin.Controllers
         }
 
         // GET: Admin/NhaSanXuatAdmin/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(String id)
         {
-            return View();
+            return View(NhaSanXuatBUS.ChiTietAdmin(id));
         }
 
         // POST: Admin/NhaSanXuatAdmin/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(String id, NhaSanXuat nsx)
         {
             try
             {
                 // TODO: Add update logic here
-
+                NhaSanXuatBUS.UpdateNSX(id, nsx);
                 return RedirectToAction("Index");
             }
             catch
@@ -67,6 +67,25 @@ namespace Startup_ShopOnline.Areas.Admin.Controllers
             }
         }
 
+        public ActionResult XoaTamThoi(String id)
+        {
+            return View(NhaSanXuatBUS.ChiTietAdmin(id));
+        }
+        [HttpPost]
+        public ActionResult XoaTamThoi(String id, NhaSanXuat nsx)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                nsx.TinhTrang = "1";
+                NhaSanXuatBUS.UpdateNSX(id, nsx);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
         // GET: Admin/NhaSanXuatAdmin/Delete/5
         public ActionResult Delete(int id)
         {
@@ -75,12 +94,12 @@ namespace Startup_ShopOnline.Areas.Admin.Controllers
 
         // POST: Admin/NhaSanXuatAdmin/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(String id, NhaSanXuat nsx)
         {
             try
             {
                 // TODO: Add delete logic here
-
+                
                 return RedirectToAction("Index");
             }
             catch
